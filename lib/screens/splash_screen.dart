f904282daf032057f2dc82c1fddb858eac6f55f8
waliuba <waliubaxavier@gmail.com>
2026-03-02 17:58:16 +0300

    splash screen implementation

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+      
+        
+        children:[
+           Lottie.asset(
+          'lib/assets/splash/Shopping Cart Loader.json',
+          width: 220,
+          height: 220,
+          fit: BoxFit.contain,
+        ),
+       const SizedBox(height: 15),
+     const Text(
+      'Chop-Shop',
+      style: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.bold
+        ),
+       ),
+     ],
+    ),
+  ),
+
+  );
+
+  
+}
+}
